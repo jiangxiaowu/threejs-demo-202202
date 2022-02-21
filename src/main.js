ef526fbee5da372e1eb5b01da6f562382f2ea4e2
Jiang <jiang@sinosoft.com.cn>
2022-02-21 11:11:23 +0800

    cute

---
+++ src/main.js
@@ -0,0 +1,59 @@
+const app = document.getElementById("app");
+const p = document.createElement("p");
+p.innerText = "write p";
+app.appendChild(p);
+
+import * as Three from 'three';
+
+console.log("Three", Three);
+
+let scene, camera, renderer, cube;
+
+function init() {
+    scene = new Three.Scene();
+
+    camera = new Three.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    )
+
+    renderer = new Three.WebGLRenderer({ antialias: true });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new Three.BoxGeometry(2, 2, 2);
+    const material = new Three.MeshBasicMaterial({ color: 0xaaafff });
+
+    // const texture = new Three.TextureLoader().load('textures/crate.gif');
+    // const material = new Three.MeshBasicMaterial({ map: texture });
+    cube = new Three.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+}
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+animate();
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (env = {}) => {
+  console.info("env", env);
+
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ];
+  
+  return {
+    mode: "development",
+    devtool: "source-map",
+    entry: path.resolve(__dirname, "src/main.js"),
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/",
+    },
+    devServer: {
+      port: 3000,
+      static: "./",
+    },
+    module: {
+      rules: [],
+    },
+    plugins: plugins,
+  };
+};
